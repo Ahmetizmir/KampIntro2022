9072fa8e8234e06f7957317f302b2c491482e086
Ahmet <Ahmet@DESKTOP-U0IRGGP>
2022-05-28 12:07:21 +0300

    28 Mayıs 2022 Ders Sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;              // geçiçi dizinin referansı eşittir item referansı   önceki elemanaları tutmak için yazıyorum bunu
+            items = new T[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }  //hangi kategriye ait mesela 1 numaralı Id Bilgisayar 2 numaralı Id Tekstil
+        public string ProductName { get; set; } //adı
+        public double UnitPrice { get; set; } //fiyatı
+        public int UnitsInStock { get; set; }  //stok adedi
+    }
+}
